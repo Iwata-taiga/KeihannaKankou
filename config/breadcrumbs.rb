4a9137baaba010c9be6d074d85b9410e032e3725
kiyoshi <kiyo7s@icloud.com>
2020-07-27 16:48:45 +0900

    完成

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -2,11 +2,6 @@ crumb :root do
   link "HOME", root_path
 end
 
-crumb :company do
-  link "会社概要", company_mains_path
-  parent :root
-end
-
 crumb :contact do
   link "お問い合わせ", contact_mains_path
   parent :root
